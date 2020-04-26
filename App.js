d4ee448aceb4b9ed5523b4adc2a3abe0ea9b78b7
P-iknow <apricotsoul@gmail.com>
2020-04-26 19:44:17 +0900

    feat: Loading

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Loading from './Loading';
+import { API_KEY } from 'react-native-dotenv';
 
 export default function App() {
-  return <Loading/>
+	console.log(API_KEY);
+	return <Loading />;
 }
--- Loading.js
+++ Loading.js
@@ -6,20 +6,20 @@ export default function Loading() {
 		<View style={style.container}>
 			<Text style={style.text}>Getting the Fokin Weather...</Text>
 		</View>
-	)
+	);
 }
 
 const style = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: "flex-end",
-		backgroundColor: "#FDF6AA",
+		justifyContent: 'flex-end',
+		backgroundColor: '#FDF6AA',
 		paddingHorizontal: 30,
 		paddingVertical: 100,
 	},
 	text: {
-		color: "#2c2c2c",
+		color: '#2c2c2c',
 		fontSize: 30,
-		fontWeight: "bold"
-	}
-})
+		fontWeight: 'bold',
+	},
+});
